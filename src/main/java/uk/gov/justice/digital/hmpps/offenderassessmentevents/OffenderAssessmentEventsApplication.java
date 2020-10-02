736dd8d22b735069e522bf117d30c0950797caa9
AstroAsh <aisha.nasir@digital.justice.gov.uk>
2020-10-02 10:53:23 +0100

    🎉 add spring boilerplate project (#1)
    - gradle
    - java11
    - snapshot 2.3.4

---
+++ src/main/java/uk/gov/justice/digital/hmpps/offenderassessmentevents/OffenderAssessmentEventsApplication.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.offenderassessmentevents;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OffenderAssessmentEventsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OffenderAssessmentEventsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/gov/justice/digital/hmpps/offenderassessmentevents/OffenderAssessmentEventsApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.digital.hmpps.offenderassessmentevents;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OffenderAssessmentEventsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
