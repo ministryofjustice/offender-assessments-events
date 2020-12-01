0845f77e74b3a99190043851641d756a161ced11
nataliewood <natalie.wood@digital.justice.gov.uk>
2020-12-01 09:26:27 +0000

    add h2 and oauth config

---
+++ src/main/kotlin/uk/gov/justice/digital/hmpps/assessments_events/config/SpringConfiguration.kt
@@ -0,0 +1,43 @@
+package uk.gov.justice.digital.hmpps.assessments_events.config
+
+import com.fasterxml.jackson.annotation.JsonInclude
+import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import uk.gov.justice.digital.hmpps.assessments_events.utils.RequestData
+
+@Configuration
+class SpringConfiguration : WebMvcConfigurer {
+
+    @Value("\${logging.uris.exclude.regex}")
+    private val excludedLogUrls : String? = null
+
+    @Bean(name = ["globalObjectMapper"])
+    @Primary
+    fun objectMapper(): ObjectMapper {
+        return ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+                .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false)
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL)
+                .setSerializationInclusion(JsonInclude.Include.NON_ABSENT)
+                .registerModules(Jdk8Module(), JavaTimeModule(), KotlinModule())
+    }
+
+    @Bean
+    fun createRequestData(): RequestData {
+        return RequestData(excludedLogUrls)
+    }
+
+    override fun addInterceptors(registry: InterceptorRegistry) {
+        registry.addInterceptor(createRequestData())
+    }
+}
