049eb83ee2d85d65c48233018f7dc57673748b3c
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-01-11 15:25:33 +0000

    fix relative path for last accessed event file

--- src/test/kotlin/uk/gov/justice/digital/hmpps/assessments_events/utils/LastAccessedEventHelperTest.kt
+++ src/test/kotlin/uk/gov/justice/digital/hmpps/assessments_events/utils/LastAccessedEventHelperTest.kt
@@ -3,16 +3,19 @@ package uk.gov.justice.digital.hmpps.assessments_events.utils
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
-import uk.gov.justice.digital.hmpps.assessments_events.integration.IntegrationTestBase
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import java.time.LocalDateTime
 
-class LastAccessedEventHelperTest : IntegrationTestBase() {
+@SpringBootTest
+@ActiveProfiles("test")
+class LastAccessedEventHelperTest {
 
   @Autowired
   lateinit var lastAccessedEvent: LastAccessedEventHelper
 
   @Test
-  fun canReadLastAccessedFromProperties() {
+  fun `Can read Last Accessed Event date from properties`() {
 
     val event = lastAccessedEvent.lastAccessedEvent()
     assertThat(event).isEqualTo(LocalDateTime.of(2020, 1, 1, 1, 1))
