e1ec631afb387b40a7e4f5e2459d68816f4dd864
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-03-22 11:47:42 +0000

    :arrow_up: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "1.1.2"
-  kotlin("plugin.spring") version "1.4.21"
-  kotlin("plugin.jpa") version "1.3.72"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
+  kotlin("plugin.spring") version "1.4.30"
+  kotlin("plugin.jpa") version "1.4.30"
 }
 
 configurations {
@@ -16,22 +16,22 @@ dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-  implementation("org.springframework.data:spring-data-relational:2.1.1")
+  implementation("org.springframework.data:spring-data-relational:2.1.6")
 
-  implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.4.RELEASE")
+  implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.4.7")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.4.7")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.4.7")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
 
-  implementation("org.apache.commons:commons-lang3:3.11")
+  implementation("org.apache.commons:commons-lang3:3.12.0")
 
   implementation(files("lib/ojdbc8-12.2.0.1.jar"))
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:6.3.3")
+  runtimeOnly("org.flywaydb:flyway-core:7.7.0")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("com.ninja-squad:springmockk:2.0.3")
+  testImplementation("com.ninja-squad:springmockk:3.0.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
 }
