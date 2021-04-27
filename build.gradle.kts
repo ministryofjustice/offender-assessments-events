98148efefb2b27d1c3c154ff9f08049532aa3cec
nataliewood <natalie.wood@digital.justice.gov.uk>
2021-04-27 10:15:53 +0100

    :arrow_up: upgrade dependencies to fix owasp check upgrade hmpps gradle plugin to 3.1.6 remove additional suppression file

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.1"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.1.6"
   kotlin("plugin.spring") version "1.4.30"
   kotlin("plugin.jpa") version "1.4.30"
 }
@@ -8,28 +8,24 @@ configurations {
   testImplementation { exclude(group = "org.junit.vintage") }
 }
 
-dependencyCheck {
-  suppressionFiles.add("hmpps-assessments-api-suppressions.xml")
-}
-
 dependencies {
   implementation("org.springframework.boot:spring-boot-starter-webflux")
   implementation("org.springframework.boot:spring-boot-starter-data-jpa")
   implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-  implementation("org.springframework.data:spring-data-relational:2.1.6")
+  implementation("org.springframework.data:spring-data-relational:2.2.0")
 
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.5.8")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.5.8")
 
   implementation("org.apache.commons:commons-lang3:3.12.0")
 
   implementation(files("lib/ojdbc8-12.2.0.1.jar"))
 
   runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.7.0")
+  runtimeOnly("org.flywaydb:flyway-core:7.8.1")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
   testImplementation("com.ninja-squad:springmockk:3.0.1")
