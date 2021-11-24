5eb4293e9d7d9ed76443643a828ecae81c184a52
Stuart Whitlam <stuart.whitlam@digital.justice.gov.uk>
2021-11-24 09:58:04 +0000

    Update Circle CI Orb, dependencies and HMPPS Spring Boot Gradle Plugin (#36)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.5"
-  kotlin("plugin.spring") version "1.5.20"
-  kotlin("plugin.jpa") version "1.5.20"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "3.3.13"
+  kotlin("plugin.spring") version "1.5.31"
+  kotlin("plugin.jpa") version "1.5.31"
 }
 
 configurations {
