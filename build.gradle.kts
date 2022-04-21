53b5e3d4503c931aef5d26e8656e27081459c2bb
nataliewood <natalie.wood@digital.justice.gov.uk>
2022-04-21 11:45:26 +0100

    :arrow_up: upgrade dependencies to fix owasp check upgrade hmpps gradle plugin to 4.1.4 suppress false positive for spring-framework

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
-  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.0"
-  kotlin("plugin.spring") version "1.6.10"
-  kotlin("plugin.jpa") version "1.6.10"
+  id("uk.gov.justice.hmpps.gradle-spring-boot") version "4.1.4"
+  kotlin("plugin.spring") version "1.6.21"
+  kotlin("plugin.jpa") version "1.6.21"
 }
 
 configurations {
@@ -20,21 +20,21 @@ dependencies {
 
   implementation("org.springframework.cloud:spring-cloud-starter-aws-messaging:2.2.6.RELEASE")
 
-  implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.6")
-  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.6")
+  implementation("org.springdoc:springdoc-openapi-ui:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-data-rest:1.6.7")
+  implementation("org.springdoc:springdoc-openapi-kotlin:1.6.7")
 
   implementation("org.apache.commons:commons-lang3:3.12.0")
 
-  implementation("com.oracle.database.jdbc:ojdbc8:12.2.0.1")
+  implementation("com.oracle.database.jdbc:ojdbc8:21.5.0.0")
 
-  runtimeOnly("com.h2database:h2:1.4.200")
-  runtimeOnly("org.flywaydb:flyway-core:7.12.0")
+  runtimeOnly("com.h2database:h2:2.1.212")
+  runtimeOnly("org.flywaydb:flyway-core:8.5.8")
 
   testImplementation("io.jsonwebtoken:jjwt:0.9.1")
-  testImplementation("com.ninja-squad:springmockk:3.0.1")
+  testImplementation("com.ninja-squad:springmockk:3.1.1")
   testImplementation("com.github.tomakehurst:wiremock-standalone:2.27.2")
-  testImplementation("org.testcontainers:junit-jupiter:1.16.0")
-  testImplementation("org.testcontainers:localstack:1.16.0")
-  testImplementation("org.awaitility:awaitility-kotlin:4.1.0")
+  testImplementation("org.testcontainers:junit-jupiter:1.17.1")
+  testImplementation("org.testcontainers:localstack:1.17.1")
+  testImplementation("org.awaitility:awaitility-kotlin:4.2.0")
 }
